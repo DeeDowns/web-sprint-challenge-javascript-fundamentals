236c382340e3bb4bee85287d041dd9199c547e1c
Dee <47228065+DeeDowns@users.noreply.github.com>
2020-06-19 12:09:40 -0400

    reviewed code, ran console.logs to see if cuboid was made correctly

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -29,7 +29,7 @@ CuboidMaker.prototype.volume = function(){
   Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
 */
 
-CuboidMaker.prototype.area = function(){
+CuboidMaker.prototype.surfaceArea = function(){
   return  2 * (this.length * this.width + this.length * this.height + this.width * this.height)
 }
 
@@ -38,10 +38,12 @@ CuboidMaker.prototype.area = function(){
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
 */
 
-const cuboidOne = new CuboidMaker(4, 5, 5)
+const cuboid = new CuboidMaker(4, 5, 5)
+console.log(cuboid)
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
 
 
